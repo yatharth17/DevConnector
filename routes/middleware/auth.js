0ea3420abfac2f4e2009e54c5663911f718e42b8
yatharth17 <yatharthkatyal4@gmail.com>
2019-09-30 13:31:16 +0530

    first commit

---
+++ routes/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+  //get token from header
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ msg: "No token,authorizationn denied" });
+  }
+
+  //verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "token is not valid" });
+  }
+};
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+connectDB();
+//Init Middleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API runnig"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/post", require("./routes/post"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/profile", require("./routes/profile"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
