e8499754887b84818c2d2c917e9b4ffb5165caed
yatharth17 <yatharthkatyal4@gmail.com>
2019-10-03 00:12:50 +0530

    commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login"
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/Login" component={Login}/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
