9498e72afd6c6c4244ea326d369b3a90aa24f35a
avikant wadhwa <avikant16@hotmail.com>
2019-10-03 08:01:29 +0530

    Delete_redudant

--- README.md
+++
@@ -1,2 +0,0 @@
-# DeveConnector
-A social media app built using MERN stack(Developed this while my udemy course with brad travesy)
--- routes/post.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//@ route  GET api/post
-//@desc    Test route
-//@access  Public
-router.get("/", (req, res) => res.send("Post route"));
-module.exports = router;
